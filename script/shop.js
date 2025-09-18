5c4d4fc9f5e44007096c6ddb2b1a87a7cd796826
dzmitrysopat <102973988+dzmitrysopat@users.noreply.github.com>
2025-09-18 22:17:10 +0300

    main

--- script/shop.js
+++ script/shop.js
@@ -2,65 +2,74 @@ const mans = [
     {
     "id": 1,
     "name": "01",
-    "price": "45",
+    "price": "115",
     "photo": "./media/compositions/mans/IMG_20250811_154329_028.jpg",
-    "photo2": "./media/compositions/mans/IMG_20250811_154332_716.jpg"
+    "photo2": "./media/compositions/mans/IMG_20250811_154332_716.jpg",
+    "description": "перец, помидоры черри, мясные чипсы, сыр косичка, колбасы высших сортов 3 вида, пастрома, зелень розмарина<br>Ящик дерево<br><br>Заказ 3-5 дней"
   },
   {
     "id": 2,
     "name": "02",
-    "price": "45",
-    "photo": "./media/compositions/mans/IMG_20250811_154651_741.jpg",
-    "photo2": "./media/compositions/mans/IMG_20250811_154709_814.jpg"
+    "price": "95",
+    "photo": "./media/compositions/mans/IMG_20250811_154709_814.jpg",
+    "photo2": "",
+    "description": "помидоры черри, мясные чипсы, колбасы высших сортов 3 вида, сыр косичка, сыр шарики, зелень розмарина. <br> Напиток в стоимость не входит<br><br>Заказ 3-5 дней"
   },
   {
     "id": 3,
     "name": "03",
-    "price": "45",
+    "price": "65",
     "photo": "./media/compositions/mans/IMG_20250811_154812_304.jpg",
-    "photo2": "./media/compositions/mans/IMG_20250811_154824_546.jpg"
+    "photo2": "./media/compositions/mans/IMG_20250811_154824_546.jpg",
+    "description": "орехи в панировке, арахис соленый, сухарики, колбасы высших сортов 3 видов, сыр косичка, мясные чипсы.Напиток в стоимость не входит. Цвет упаковки можно обсудить под запрос.<br><br>Заказ 3-5 дней"
   },
   {
     "id": 4,
-    "name": "04",
-    "price": "45",
+    "name": "04 Бокс",
+    "price": "57",
     "photo": "./media/compositions/mans/IMG_20250811_155451_426.jpg",
-    "photo2": "./media/compositions/mans/IMG_20250811_155447_729.jpg"
+    "photo2": "./media/compositions/mans/IMG_20250811_155447_729.jpg",
+    "description": "орехи в панировке, миндаль, фундук, соленый арахис, мясные чипсы, колбаски  3-4 вида, сухарики. <br>Напиток в стоимость не входит. <br> Размер бокса 20*20<br><br>Заказ 3-5 дней"
   },
   {
     "id": 5,
     "name": "05",
     "price": "45",
     "photo": "./media/compositions/mans/IMG_20250811_155417_222.jpg",
-    "photo2": "./media/compositions/mans/IMG_20250811_155412_500.jpg"
+    "photo2": "./media/compositions/mans/IMG_20250811_155412_500.jpg",
+    "description": ""
   },
   {
     "id": 6,
     "name": "06",
-    "price": "45",
+    "price": "150",
     "photo": "./media/compositions/mans/IMG_20250811_160041_778.jpg",
-    "photo2": "./media/compositions/mans/IMG_20250811_160039_725.jpg"
+    "photo2": "./media/compositions/mans/IMG_20250811_160039_725.jpg",
+    "description": "перец Чили, помидоры черри, мясные чипсы, колбасы высших сортов 3-4 вида, паструма, сыр косичка, розмарин.<br> Ящик дерево.<br> Напиток в стоимость не входит <br><br>Заказ 3-5 дней"
   },
   {
     "id": 7,
     "name": "07",
-    "price": "45",
+    "price": "93",
     "photo": "./media/compositions/mans/IMG_20250811_155706_925.jpg",
-    "photo2": "./media/compositions/mans/IMG_20250811_155708_876.jpg"
+    "photo2": "./media/compositions/mans/IMG_20250811_155708_876.jpg",
+    "description": "помидоры черри, сыр косичка, сыр палочки, колбаска божале в красном вине, мясные чипсы, колбасы высших сортов 2 видов.<br> Ящик дерево.<br> Напиток в стоимость не входит<br><br>Заказ 3-5 дней "
   },
   {
     "id": 8,
     "name": "08",
-    "price": "45",
+    "price": "135",
     "photo": "./media/compositions/mans/IMG_20250811_155609_021.jpg",
     "photo2": "./media/compositions/mans/IMG_20250811_155606_992.jpg",
+    "description": "помидоры черри, колбасы высших сортов 3-4 вида, сыр косичка, паструма, мясные чипсы, зелень розмарина.<br> Ящик дерево.<br> Напиток в стоимость не входит <br><br>Заказ 3-5 дней"
   },
   {
     "id": 9,
-    "name": "09",
-    "price": "45",
+    "name": "09 Мини конверт",
+    "price": "60",
     "photo": "./media/compositions/mans/IMG_20250811_155331_879.jpg",
-    "photo2": "./media/compositions/mans/IMG_20250811_155328_054.jpg"
+    "photo2": "./media/compositions/mans/IMG_20250811_155328_054.jpg",
+    "description": "орехи в панировке, сыр косичка, помидоры черри, мясные чипсы, колбаски 3 видов, зелень. <br> Напиток в стоимость не входит. <br><br>Заказ 3-5 дней"
   },
   {
     "id": 10,
@@ -69,40 +78,25 @@ const mans = [
     "photo": "./media/compositions/mans/IMG_20250811_170439_976.jpg",
   },
   {
-    "id": 11,
-    "name": "011",
-    "price": "45",
-    "photo": "./media/compositions/mans/IMG_20250811_155204_068.jpg",
-  },
-  {
-    "id": 12,
-    "name": "012",
-    "price": "45",
-    "photo": "./media/compositions/mans/IMG_20250811_155145_809.jpg",
-  },
-  {
     "id": 13,
-    "name": "013",
-    "price": "45",
+    "name": "013 Мужской бокс",
+    "price": "60",
     "photo": "./media/compositions/mans/IMG_20250811_155127_578.jpg",
+    "description": "мясные чипсы, сухарики, орехи в панировке, рыбка жёлтый полосатик, колбаски 2 видов.<br> Бокс дерево размеры 25*25. <br>Напиток в стоимость не входит.<br><br>Заказ 3-5 дней "
   },
   {
     "id": 14,
-    "name": "014",
+    "name": "014 Букет круглый",
     "price": "45",
     "photo": "./media/compositions/mans/IMG_20250811_155111_220.jpg",
-  },
-  {
-    "id": 15,
-    "name": "015",
-    "price": "45",
-    "photo": "./media/compositions/mans/IMG_20250811_155056_922.jpg",
+    "description": ""
   },
   {
     "id": 16,
-    "name": "016",
-    "price": "45",
+    "name": "016 Ящик ассорти 'Премиум'",
+    "price": "180",
     "photo": "./media/compositions/mans/IMG_20250811_155036_754.jpg",
+    "description": "сыр палочки, колбаса Божоле, паструма, Парма с плесенью, кабандосы классик, мясные чипсы, сьедобная зелень. Форель, вомер, жёлтый полосатик, снежки лосось, палочки минтай. <br>Ящик дерево.<br> Напиток в стоимость не входит <br><br>Заказ 3-5 дней"
   },
   {
     "id": 17,
@@ -118,28 +112,32 @@ const womans = [
     "name": "01",
     "price": "45",
     "photo": "./media/compositions/woman/IMG_20250811_165419_941.jpg",
-    "photo2": "./media/compositions/woman/IMG_20250811_165420_531.jpg"
+    "photo2": "./media/compositions/woman/IMG_20250811_165420_531.jpg",
+    "description": ""
   },
   {
     "id": 2,
     "name": "02",
     "price": "45",
     "photo": "./media/compositions/woman/IMG_20250811_165420_835.jpg",
-    "photo2": "./media/compositions/woman/IMG_20250811_165419_877.jpg"
+    "photo2": "./media/compositions/woman/IMG_20250811_165419_877.jpg",
+    "description": ""
   },
   {
     "id": 3,
     "name": "03",
     "price": "45",
     "photo": "./media/compositions/woman/IMG_20250811_165420_448.jpg",
-    "photo2": "./media/compositions/woman/IMG_20250811_165419_986.jpg"
+    "photo2": "./media/compositions/woman/IMG_20250811_165419_986.jpg",
+    "description": ""
   },
   {
     "id": 4,
     "name": "04",
     "price": "45",
     "photo": "./media/compositions/woman/IMG_20250811_165423_897.jpg",
-    "photo2": "./media/compositions/woman/IMG_20250811_165423_888.jpg"
+    "photo2": "./media/compositions/woman/IMG_20250811_165423_888.jpg",
+    "description": ""
   }
 ];
 
@@ -149,28 +147,32 @@ const child = [
     "name": "01",
     "price": "45",
     "photo": "./media/compositions/child/IMG_20250811_165236_260.jpg",
-    "photo2": "./media/compositions/child/IMG_20250811_165237_045.jpg"
+    "photo2": "./media/compositions/child/IMG_20250811_165237_045.jpg",
+    "description": ""
   },
   {
     "id": 2,
     "name": "02",
     "price": "45",
     "photo": "./media/compositions/child/IMG_20250811_165236_939.jpg",
-    "photo2": "./media/compositions/child/IMG_20250811_165236_961.jpg"
+    "photo2": "./media/compositions/child/IMG_20250811_165236_961.jpg",
+    "description": ""
   },
   {
     "id": 3,
     "name": "03",
     "price": "45",
     "photo": "./media/compositions/child/IMG_20250811_165236_448.jpg",
-    "photo2": ""
+    "photo2": "",
+    "description": ""
   },
   {
     "id": 4,
     "name": "04",
     "price": "45",
     "photo": "./media/compositions/child/IMG_20250811_165236_726.jpg",
-    "photo2": "./media/compositions/child/IMG_20250811_165236_340.jpg"
+    "photo2": "./media/compositions/child/IMG_20250811_165236_340.jpg",
+    "description": ""
   }
 ];
 
@@ -181,6 +183,7 @@ const Fruits = [
     "price": "45",
     "photo": "./media/compositions/oldFruits/IMG_20250811_163321_594.jpg",
     "photo2": "./media/compositions/oldFruits/IMG_2343.JPG",
+    "description": "",
     "photo3": "./media/compositions/oldFruits/IMG_20250811_163310_285.jpg"
 
   },
@@ -189,42 +192,48 @@ const Fruits = [
     "name": "02",
     "price": "45",
     "photo": "./media/compositions/oldFruits/IMG_2340.JPG",
-    "photo2": ""
+    "photo2": "",
+    "description": ""
   },
   {
     "id": 3,
     "name": "03",
     "price": "45",
     "photo": "./media/compositions/oldFruits/IMG_2341.JPG",
-    "photo2": "./media/compositions/oldFruits/IMG_2342.JPG"
+    "photo2": "./media/compositions/oldFruits/IMG_2342.JPG",
+    "description": ""
   },
   {
     "id": 4,
     "name": "04",
     "price": "45",
     "photo": "./media/compositions/oldFruits/IMG_20250811_163419_162.jpg",
-    "photo2": "./media/compositions/oldFruits/IMG_20250811_163423_905.jpg"
+    "photo2": "./media/compositions/oldFruits/IMG_20250811_163423_905.jpg",
+    "description": ""
   },
   {
     "id": 5,
     "name": "05",
     "price": "45",
     "photo": "./media/compositions/oldFruits/IMG_20250811_163445_820.jpg",
-    "photo2": "./media/compositions/oldFruits/IMG_20250811_163448_068.jpg"
+    "photo2": "./media/compositions/oldFruits/IMG_20250811_163448_068.jpg",
+    "description": ""
   },
   {
     "id": 6,
     "name": "06",
     "price": "45",
     "photo": "./media/compositions/oldFruits/IMG_20250811_163500_639.jpg",
-    "photo2": "./media/compositions/oldFruits/IMG_20250811_163503_230.jpg"
+    "photo2": "./media/compositions/oldFruits/IMG_20250811_163503_230.jpg",
+    "description": ""
   },
   {
     "id": 7,
     "name": "07",
     "price": "45",
     "photo": "./media/compositions/oldFruits/IMG_20250811_163512_199.jpg",
-    "photo2": "./media/compositions/oldFruits/IMG_20250811_163514_812.jpg"
+    "photo2": "./media/compositions/oldFruits/IMG_20250811_163514_812.jpg",
+    "description": ""
   }
 ];
 
@@ -232,51 +241,58 @@ const Premium = [
     {
     "id": 1,
     "name": "01",
-    "price": "45",
+    "price": "305",
     "photo": "./media/compositions/premium/IMG_20250811_163732_860.jpg",
-    "photo2": "./media/compositions/premium/IMG_20250811_163732_956.jpg"
+    "photo2": "./media/compositions/premium/IMG_20250811_163732_956.jpg",
+    "description": "<br>Цветы: эустома, хризантема, зелень. <br><br>Сухофрукты: инжир, курага, кумкват, ананас, кизил, манго, финики, хурма целая. <br><br>Орехи: пекан, макадами, миндаль, фундук, фундук в белом шоколаде. <br><br> Диаметр съедобной части около 45см <br>Цена без учёта напитка. <br><br>Заказ 5-7 дней"
   },
   {
     "id": 2,
     "name": "02",
-    "price": "45",
+    "price": "290",
     "photo": "./media/compositions/premium/IMG_20250811_163701_453.jpg",
-    "photo2": "./media/compositions/premium/IMG_20250811_163728_975.jpg"
+    "photo2": "./media/compositions/premium/IMG_20250811_163728_975.jpg",
+    "description": "<br>Сухофрукты: мед 3шт, манго, курага, кумкват, финик, инжир двух видов, вяленая груша (по сезону), маракуйа, хурма вяленая целая, персик, кубики манго. <br><br>Орехи: пекан, макадами, фундук, миндаль. <br><br>Размер сьедобной части около 40-45см<br><br>Заказ 5-7 дней"
   },
   {
     "id": 3,
     "name": "03",
     "price": "45",
     "photo": "./media/compositions/premium/IMG_20250811_163733_604.jpg",
-    "photo2": "./media/compositions/premium/IMG_20250811_163733_055.jpg"
+    "photo2": "./media/compositions/premium/IMG_20250811_163733_055.jpg",
+    "description": ""
   },
   {
     "id": 4,
     "name": "04",
-    "price": "45",
+    "price": "285",
     "photo": "./media/compositions/premium/IMG_20250811_163733_104.jpg",
-    "photo2": "./media/compositions/premium/IMG_20250811_163733_380.jpg"
+    "photo2": "./media/compositions/premium/IMG_20250811_163733_380.jpg",
+    "description": "<br>Сухофрукты: Манго двух видов, курага, кумкват, инжир, финики обычный и королевский, ананас двух видов, хурма целая, кизил. <br><br>Орехи: пекан, макадами, миндаль, фундук в белом шоколаде.<br><br>Размеры сьедобной части до 40см <br><br>Заказ 5-7 дней"
   },
   {
     "id": 5,
     "name": "05",
-    "price": "45",
+    "price": "250",
     "photo": "./media/compositions/premium/IMG_20250811_163733_123.jpg",
-    "photo2": "./media/compositions/premium/IMG_20250811_163733_553.jpg"
+    "photo2": "./media/compositions/premium/IMG_20250811_163733_553.jpg",
+    "description": "<br>Цветы: эустома, эвкалипт <br><br>Сухофрукты: Хурма целая, манго, ананас, курага, кумкват, инжир, палочки корицы, финики. <br><br>Орехи: пекан, макадами, фундук, фундук в белом шоколаде, миндаль.<br><br>Диаметр съедобной части около 40см <br><br>Заказ 5-7 дней "
   },
   {
     "id": 6,
     "name": "06",
-    "price": "45",
+    "price": "300",
     "photo": "./media/compositions/premium/IMG_20250811_163733_603.jpg",
-    "photo2": "./media/compositions/premium/IMG_20250811_163732_968.jpg"
+    "photo2": "./media/compositions/premium/IMG_20250811_163732_968.jpg",
+    "description": "<br>Цветы: роза кустовая, хризантема, зелень <br><br>Сухофрукты: инжир, кумкват, курага, манго, хурма целая, персик, кубики манго.<br><br> Орехи: пекан, макадами, миндаль, фундук, фундук в белом шоколаде. <br><br>Диаметр съедобной части около 45см <br><br>Заказ 5-7 дней"
   },
   {
     "id": 7,
     "name": "07",
-    "price": "45",
+    "price": "165",
     "photo": "./media/compositions/premium/IMG_20250811_163728_557.jpg",
-    "photo2": "./media/compositions/premium/IMG_20250811_163728_769.jpg"
+    "photo2": "./media/compositions/premium/IMG_20250811_163728_769.jpg",
+    "description": "<br>Цветы: роза кустовая, гипсофил, зелень <br><br> Сухофрукты: манго, инжир, финики, курага, кумкват, фундук в белом шоколаде, Рафаэлло. <br><br> Диаметр съедобной части около 30см <br><br> Заказ 5-7 дней "
   }
 ];
 
@@ -302,6 +318,9 @@ function displayProducts(products, containerId) {
             ${product.photo2 && product.photo2.trim() !== "" ? '<div class="swiper-pagination"></div>' : ''}
             <p class="name">${product.name}</p>
             <span class="price">${product.price} руб.</span>
+            <span class="description"> 
+            <span class="price">Состав:</span>
+            ${product.description}</span>
         `;
 
         container.appendChild(card);
